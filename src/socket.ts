7b0cbcc3872ba25bd1eea2f23bcee87354a51518
Osama <usamatariq10048@gmail.com>
2023-10-23 17:08:02 +0500

    source url changed

--- src/socket.ts
+++ src/socket.ts
@@ -3,8 +3,8 @@ import { Socket, io } from "socket.io-client";
 // const SERVER_IP = "socket-koyeb-ekobyte.koyeb.app"; // e.g., '10.13.1.215'
 // const SERVER_PORT = 3000;
 // const URL = `https://${SERVER_IP}:${SERVER_PORT}`;
-const URL = `http://localhost:8000`;
-// const URL = `https://socket-koyeb-ekobyte.koyeb.app`;
+// const URL = `http://localhost:8000`;
+const URL = `https://socket-koyeb-ekobyte.koyeb.app`;
 interface SocketData extends Socket {
   userID?: string;
   username?: string;
